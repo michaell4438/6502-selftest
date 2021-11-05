9d5fec2f5270479d0eccc04a9a284df47cb14ca3
AndroidUser4438 <michael@lachut.com>
2021-11-05 17:16:12 -0400

    Major Changes to post.asm

--- post.asm
+++ post.asm
@@ -1,14 +1,17 @@
 TEST_ZP_ADDR = $0F
 TEST_ZP_DAT = %11001001
-ZP_SUCCESS_MOD = %0010000
+ZP_SUCCESS_MOD = %0001000
 
 TEST_STACK_DAT = %01101100
-STACK_SUCCESS_MOD = %0001000
+STACK_SUCCESS_MOD = %0010000
 
 TEST_GEN_ADDR = $106F
 TEST_GEN_DAT = %01011010
 GEN_SUCCESS_MOD = %0000100
 
+
+post_fail:
+  jmp post_fail
 post:
   lda #%11111111
   sta DDRB
@@ -18,64 +21,63 @@ post:
   lda #%10000000
   sta PORTB
 
-  tax
-  txa
-
   ora #%01000000
   sta PORTB
 
-  tax
-
-  lda #TEST_ZP_DAT
-  sta TEST_ZP_ADDR
-
-  lda TEST_ZP_ADDR
-  cmp #TEST_ZP_DAT
-
-  beq ZP_SUCCESS
-  jmp stage2
-
-ZP_SUCCESS:
-  txa
-  ora #ZP_SUCCESS_MOD
-
-  sta PORTB
-  jmp stage2
-
-stage2:
-  tax
+  pha
 
   lda #TEST_STACK_DAT
   pha
-
+  lda #$FF
   pla
+
   cmp #TEST_STACK_DAT
 
-  beq STACK_SUCCESS
-  jmp stage3
+  beq stage2
+  jmp post_fail
 
-STACK_SUCCESS:
-  txa  
+stage2:
+  pla
+
   ora #STACK_SUCCESS_MOD
-
   sta PORTB
-  jmp stage3
+
+  pha 
+
+  lda #TEST_ZP_DAT
+  sta TEST_ZP_ADDR
+  lda #$FF
+  
+  lda TEST_ZP_ADDR
+
+  cmp #TEST_ZP_DAT
+
+  beq stage3
+  jmp post_fail
 
 stage3:
-  tax
+  pla
+
+  ora #ZP_SUCCESS_MOD
+  sta PORTB
+
+  pha 
 
   lda #TEST_GEN_DAT
   sta TEST_GEN_ADDR
-
+  lda #$FF
+  
   lda TEST_GEN_ADDR
+
   cmp #TEST_GEN_DAT
 
-  beq GEN_SUCCESS
-  jmp return_post
+  beq stage4
+  jmp post_fail
 
-GEN_SUCCESS:
-  txa  
+stage4:
+  pla
+
   ora #GEN_SUCCESS_MOD
-
   sta PORTB
+
   jmp return_post
